013533b602cd8abc09de8c1abfd71f1b5515962a
Brena Barradas <brenaraiaracb@gmail.com>
2024-12-27 17:45:38 -0300

    modificando o header

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import {Post} from "./Post";
+
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header/>
+
      <Post
         author="Diego Fernandes"
         content="Mollit enim ad qui labore esse sit. Sit in laborum anim sunt nostrud ut deserunt commodo. Eu Lorem dolor in laborum. Voluptate consequat magna tempor laborum do et. Amet cupidatat sint anim est ut fugiat adipisicing magna tempor reprehenderit pariatur quis et."
