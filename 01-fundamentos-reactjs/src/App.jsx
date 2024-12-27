c57545f1ced2039e03da5f1ccbf49f54f82ab93c
Brena Barradas <brenaraiaracb@gmail.com>
2024-12-27 11:57:54 -0300

    adicionando o projeto

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+import {Post} from "./Post";
+
+export function App() {
+  return (
+    <div>
+     <Post
+        author="Diego Fernandes"
+        content="Mollit enim ad qui labore esse sit. Sit in laborum anim sunt nostrud ut deserunt commodo. Eu Lorem dolor in laborum. Voluptate consequat magna tempor laborum do et. Amet cupidatat sint anim est ut fugiat adipisicing magna tempor reprehenderit pariatur quis et."
+      />
+      <Post
+       author="Gabriel Buzzi"
+       content="Um novo post muito legal"
+       />
+     </div>    
+  )
+}
+
