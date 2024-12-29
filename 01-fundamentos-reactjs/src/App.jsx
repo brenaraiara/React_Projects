0c384b48814238be02896db7b4f80fc717d033f4
Brena Barradas <brenaraiaracb@gmail.com>
2024-12-28 22:35:38 -0300

    adicionando a sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,20 @@
 import { Header } from "./components/Header";
 import {Post} from "./Post";
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header/>
 
-     <Post
+     <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+      <Post
         author="Diego Fernandes"
         content="Mollit enim ad qui labore esse sit. Sit in laborum anim sunt nostrud ut deserunt commodo. Eu Lorem dolor in laborum. Voluptate consequat magna tempor laborum do et. Amet cupidatat sint anim est ut fugiat adipisicing magna tempor reprehenderit pariatur quis et."
       />
@@ -16,7 +22,9 @@ export function App() {
        author="Gabriel Buzzi"
        content="Um novo post muito legal"
        />
-     </div>    
+      </main>
+     </div>
+    </div>    
   )
 }
 
