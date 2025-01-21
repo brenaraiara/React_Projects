9fd6cac237c88d8d2032010050a67ab4ffdc32e3
Brena Barradas <brenaraiaracb@gmail.com>
2025-01-20 23:22:11 -0300

    Propriedades do Post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+      author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1594136976553-38699ae9047c?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+  },
+  content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋,'},
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    { type: 'link', content: 'jane.design/doctorcare'}, 
+  ],
+  publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+  id: 2,
+    author: {
+    avatarUrl: 'https://plus.unsplash.com/premium_photo-1682125148697-581ce1425d5e?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Maik Brito',
+    role: 'Educator @Rocketseat'
+},
+content: [
+  { type: 'paragraph', content: 'Fala galeraa 👋,'},
+  { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+  { type: 'link', content: 'jane.design/doctorcare'}, 
+],
+publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
 
 export function App() {
   return (
@@ -15,14 +46,15 @@ export function App() {
      <div className={styles.wrapper}>
       <Sidebar />
       <main>
-      <Post
-        author="Diego Fernandes"
-        content="Mollit enim ad qui labore esse sit. Sit in laborum anim sunt nostrud ut deserunt commodo. Eu Lorem dolor in laborum. Voluptate consequat magna tempor laborum do et. Amet cupidatat sint anim est ut fugiat adipisicing magna tempor reprehenderit pariatur quis et."
-      />
-      <Post
-       author="Gabriel Buzzi"
-       content="Um novo post muito legal"
-       />
+        {posts.map(posts => {
+            return (
+              <Post 
+              author={posts.author}
+              content={posts.content}
+              publishedAt={posts.publishedAt}
+              />
+        )
+        })}
       </main>
      </div>
     </div>    
